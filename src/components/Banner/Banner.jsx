618c5dad65c3ca29000f116b03c8a97d65c0425d
raaz1212 <araj30728@gmail.com>
2023-05-18 07:47:46 +0600

    banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,63 @@
 import React from "react";
 
 const Banner = () => {
-  return <div></div>;
+  return (
+    <div className="carousel w-full h-96">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img
+          src="https://i.ibb.co/fYLbhM1/DC-Direct-Banner.jpg"
+          className="w-full"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src="https://i.ibb.co/nsmDGJK/download.jpg" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img
+          src="https://i.ibb.co/VC5ZS6q/maxresdefault.jpg"
+          className="w-full"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img
+          src="https://i.ibb.co/PFp396q/Mc-Farlane-Toys-DC-Multiverse-SDCC-2020-2-Packs-2.jpg"
+          className="w-full"
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Banner from "../Banner/Banner";
+import GallerySection from "../GallerySection/GallerySection";
+import ShopByCategory from "../ShopByCategory/ShopByCategory";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Banner />
+      <GallerySection />
+      <ShopByCategory />
+    </div>
+  );
+};
+
+export default HomePage;
