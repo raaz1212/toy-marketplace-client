910ac3f3599f973bc0d3bfa03aaa3248a8eec91c
raaz1212 <araj30728@gmail.com>
2023-05-20 21:21:37 +0600

    comment and ratings

--- src/components/CustomerReview/CustomerReview.jsx
+++ src/components/CustomerReview/CustomerReview.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const CustomerReview = () => {
+  const [comment, setComment] = useState("");
+  const [rating, setRating] = useState(0);
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleRatingChange = (event) => {
+    setRating(Number(event.target.value));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform submission logic
+    // You can also display a toast or perform any other action here
+
+    // Show toast notification
+    toast.success("Comment submitted successfully");
+
+    // Clear comment field
+    setComment("");
+    setRating(0);
+  };
+
   const reviews = [
     {
       id: 1,
@@ -36,7 +62,7 @@ const CustomerReview = () => {
   return (
     <section className="bg-gray-100 py-12 px-6">
       <div className="max-w-3xl mx-auto text-center">
-        <h2 className="text-3xl font-bold mb-8">What Our Customer Say</h2>
+        <h2 className="text-3xl font-bold mb-8">What Our Customers Say</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {reviews.map((review) => (
             <motion.div
@@ -82,7 +108,37 @@ const CustomerReview = () => {
             </motion.div>
           ))}
         </div>
+        <div className="mt-8">
+          <form onSubmit={handleSubmit}>
+            <div className="flex items-center mb-4">
+              {[1, 2, 3, 4, 5].map((value) => (
+                <FontAwesomeIcon
+                  key={value}
+                  icon={faStar}
+                  className={`text-yellow-400 cursor-pointer ${
+                    value <= rating ? "text-yellow-600" : ""
+                  }`}
+                  onClick={() => setRating(value)}
+                />
+              ))}
+            </div>
+            <textarea
+              className="w-full bg-white px-4 py-2 mb-4 rounded-md resize-none"
+              placeholder="Enter your comment"
+              value={comment}
+              onChange={handleCommentChange}
+            ></textarea>
+            <button
+              type="submit"
+              className="btn btn-primary text-black"
+              disabled={rating === 0 || comment.trim() === ""}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
+      <ToastContainer />
     </section>
   );
 };
